df762936f181cadc0ce691d08426fa60f8fe1559
Justine Sprinkle <justine.sprinklee@gmail.com>
2018-07-24 15:20:16 -0700

    added comments

--- fine_pront/fine_print.py
+++ fine_pront/fine_print.py
@@ -1,13 +1,16 @@
 import string
 # import json
 
+#list of key terms
 terms = ["data", "camera", "microphone", "audio", "billing", "tracking", "location", "organisation", "organisation's"]
 
+#open text file
 doc = open("fp.txt", "r")
 docData = doc.read()
 docData = list(docData.split(" "))
 doc.close()
 
+#function to remove punctuation
 def remove_punctuation(value):
     result = ""
     for c in value:
@@ -15,14 +18,21 @@ def remove_punctuation(value):
         if c not in string.punctuation:
             result += c
     return result
-print(docData)
+
+#goes throgh each word in list
 for x in range(len(docData)):
+
+#makes it lower and removes punctuation
     word = docData[x].lower()
     waye = remove_punctuation(word)
+
+#compare word to each key term
     for i in range(len(terms)):
+#TODO: fix semicolon bug
         if word.strip() == terms[i]:
             docData[x] = word.upper()
         if waye.strip() == terms[i]:
             docData[x] = word.upper()
 
+#joins list into string
 print(" ".join(docData))
