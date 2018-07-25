9395494594d7e913af233f65a959ca315d50d6a9
Shayna Chambless <shaychamblessshay@gmail.com>
2018-07-25 13:32:47 -0700

    created new txt files and functions
    you can now look at what you want to see

--- fine_pront/fine_print.py
+++ fine_pront/fine_print.py
@@ -1,38 +1,33 @@
 import string
-# import json
+import finePrintFunctions as fpFxs
 
-#list of key terms
-terms = ["data", "camera", "microphone", "audio", "billing", "tracking", "location", "organisation", "organisation's"]
+def main():
+    #open text file
+    doc = open("fp.txt", "r")
+    docData = doc.read()
+    docData = list(docData.split(" "))
+    doc.close()
 
-#open text file
-doc = open("fp.txt", "r")
-docData = doc.read()
-docData = list(docData.split(" "))
-doc.close()
+    termsDoc = open("DATA.txt", "r")
+    keyTerms = termsDoc.readlines()
+    termsDoc.close()
+    print("DATA\nUSER\nCAMERA\nMICROPHONE\nBILLING\nLOCATION\nALL\n")
+    specTerm = input("What would you like to look for? ")
+    if specTerm == "DATA":
+        poop = fpFxs.find_DATA(docData)
+    elif specTerm == "ALL":
+        poop = fpFxs.find_ALL(docData)
+    elif specTerm == "LOCATION":
+        poop = fpFxs.find_LOCATION(docData)
+    elif specTerm == "BILLING":
+        poop = fpFxs.find_BILLING(docData)
+    elif specTerm == "CAMERA":
+        poop = fpFxs.find_CAMERA(docData)
+    elif specTerm == "USER":
+        poop = fpFxs.find_USER(docData)
+    elif specTerm == "MICROPHONE":
+        poop = fpFxs.find_MICROPHONE(docData)
 
-#function to remove punctuation
-def remove_punctuation(value):
-    result = ""
-    for c in value:
-        # If char is not punctuation, add it to the result.
-        if c not in string.punctuation:
-            result += c
-    return result
-
-#goes throgh each word in list
-for x in range(len(docData)):
-
-#makes it lower and removes punctuation
-    word = docData[x].lower()
-    waye = remove_punctuation(word)
-
-#compare word to each key term
-    for i in range(len(terms)):
-#TODO: fix semicolon bug
-        if word.strip() == terms[i]:
-            docData[x] = word.upper()
-        if waye.strip() == terms[i]:
-            docData[x] = word.upper()
-
-#joins list into string
-print(" ".join(docData))
+    print(" ".join(poop))
+if __name__ == '__main__':
+    main()
