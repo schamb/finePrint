34afefc906ef9aac1f7963745015a34393252c46
Shayna Chambless <shaychamblessshay@gmail.com>
2018-07-25 15:16:06 -0700

    changed

--- fine_pront/fine_print.py
+++ fine_pront/fine_print.py
@@ -11,23 +11,26 @@ def main():
     termsDoc = open("DATA.txt", "r")
     keyTerms = termsDoc.readlines()
     termsDoc.close()
-    print("DATA\nUSER\nCAMERA\nMICROPHONE\nBILLING\nLOCATION\nALL\n")
-    specTerm = input("What would you like to look for? ")
-    if specTerm == "DATA":
-        poop = fpFxs.find_DATA(docData)
-    elif specTerm == "ALL":
-        poop = fpFxs.find_ALL(docData)
-    elif specTerm == "LOCATION":
-        poop = fpFxs.find_LOCATION(docData)
-    elif specTerm == "BILLING":
-        poop = fpFxs.find_BILLING(docData)
-    elif specTerm == "CAMERA":
-        poop = fpFxs.find_CAMERA(docData)
-    elif specTerm == "USER":
-        poop = fpFxs.find_USER(docData)
-    elif specTerm == "MICROPHONE":
-        poop = fpFxs.find_MICROPHONE(docData)
+    while True:
+        print("DATA\nUSER\nCAMERA\nMICROPHONE\nBILLING\nLOCATION\nALL\nSTOP\n")
+        specTerm = input("What would you like to look for? ")
+        if specTerm == "DATA":
+            poop = fpFxs.find_DATA(docData)
+        elif specTerm == "ALL":
+            poop = fpFxs.find_ALL(docData)
+        elif specTerm == "LOCATION":
+            poop = fpFxs.find_LOCATION(docData)
+        elif specTerm == "BILLING":
+            poop = fpFxs.find_BILLING(docData)
+        elif specTerm == "CAMERA":
+            poop = fpFxs.find_CAMERA(docData)
+        elif specTerm == "USER":
+            poop = fpFxs.find_USER(docData)
+        elif specTerm == "MICROPHONE":
+            poop = fpFxs.find_MICROPHONE(docData)
+        elif specTerm == "STOP":
+            break
 
-    print(" ".join(poop))
+        print(" ".join(poop))
 if __name__ == '__main__':
     main()
