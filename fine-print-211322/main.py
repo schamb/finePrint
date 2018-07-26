85dee3bbe12c942201d0489a7a11d85f044398aa
Justine Sprinkle <justine.sprinklee@gmail.com>
2018-07-26 10:38:33 -0700

    more input code

--- fine-print-211322/main.py
+++ fine-print-211322/main.py
@@ -18,7 +18,7 @@ import webapp2, jinja2, os
 from google.appengine.ext import ndb
 
 template_directory = os.path.join(os.path.dirname(__file__),'templates')
-jinja_environment = jinja2.Enviornment(loader = jinja2.FilsSystemLoader(template_directory))
+jinja_environment = jinja2.Environment(loader = jinja2.FilsSystemLoader(template_directory))
 
 class User(ndb.Model):
     privacy_policy = ndb.StringProperty()
@@ -41,7 +41,7 @@ class OutputHandler(webapp2.RequestHandler):
 
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
-    ('/addcompany', UserInput)
+    ('/', MainHandler),
+    ('/addcompany', UserInput),
     ('/companyname', OutputHandler)
 ], debug=True)
